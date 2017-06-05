10017d8ddce1737415be83e5f38b03ee19a87ca8
Misshel <jenmish094@gmail.com>
2017-06-04 21:25:19 -0500

    funcionalidad de los botones pequeños

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,16 +6,42 @@ $(document).ready(function (){
         .next('div').fadeIn(1000)
         .end().appendTo('#slider');
       };
-    $("#after").click(function(){
+
+// controles de botones de flechas
+    $("#before").click(function(){
       changeDiv();
       clearInterval(interval);
       interval= setInterval(changeDiv,5000);
     });
-    $("#before").click(function(){
+    $("#after").click(function(){
       $('#slider div:first-child').fadeOut(3000);
-      $('#slider div:last-child').fadeIn(3000).prependTo('#slider');
+      $('#slider div:last-child').fadeIn(1000).prependTo('#slider');
       clearInterval(interval);
       interval= setInterval(changeDiv,5000);
     });
 
+// controles de botones circulares
+    $("#1b").click(function(){
+      $("#slider div:first-child").fadeOut(1000);
+        $('#1img').fadeIn(1000);
+       clearInterval(interval);
+      interval= setInterval(changeDiv,5000);
+    });
+    $("#2b").click(function(){
+      $("#slider div:first-child").fadeOut(1000);
+        $('#2img').fadeIn(1000);
+       clearInterval(interval);
+    });
+    $("#3b").click(function(){
+      $("#slider div:first-child").fadeOut(1000);
+        $('#3img').fadeIn(1000);
+    });
+    $("#4b").click(function(){
+      $("#slider div:first-child").fadeOut(1000);
+        $('#4img').fadeIn(1000);
+    });
+    $("#5b").click(function(){
+      $("#slider div:first-child").fadeOut(1000);
+        $('#5img').fadeIn(1000);
+    });
 });
