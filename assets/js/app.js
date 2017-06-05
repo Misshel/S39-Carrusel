c7ef1419068e388267db7b428bfb7f72137104ec
Misshel <jenmish094@gmail.com>
2017-06-04 20:09:17 -0500

    funcionalidad automatica y control de flechas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,21 @@
+$(document).ready(function (){
+    $('#slider div:gt(0)').hide();
+    var interval= setInterval(changeDiv, 5000);
+    function changeDiv(){
+      $('#slider div:first-child').fadeOut(3000)
+        .next('div').fadeIn(1000)
+        .end().appendTo('#slider');
+      };
+    $("#after").click(function(){
+      changeDiv();
+      clearInterval(interval);
+      interval= setInterval(changeDiv,5000);
+    });
+    $("#before").click(function(){
+      $('#slider div:first-child').fadeOut(3000);
+      $('#slider div:last-child').fadeIn(3000).prependTo('#slider');
+      clearInterval(interval);
+      interval= setInterval(changeDiv,5000);
+    });
 
+});
